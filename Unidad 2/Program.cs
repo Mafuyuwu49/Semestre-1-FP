63b78bd288b6f32e744f23b67740f33c0bfd91eb
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-09-29 15:00:19 -0600

    P_1
    //comentario en una linea
    /* comentarios en bloque:
    * Console writeLine es para imprimir
    * para imprimir texto sin variable
    * tiene que ir entre " " comi*/
    using System;
    namespace p-1
        {
            internal class program // paso 1
            {
                static void main(string[] args)
                {
                    //DECLARAR VARIABLES
                    string nombre = ("Hector");
                    //imprimir nombre referenciado a una variable
                    Console.WriteLine("Hector");
                    //cada lineas debe finalizar con ; punto y coma
                }
            }
        }

---
+++ Unidad 2/Program.cs
@@ -0,0 +1,21 @@
+﻿//comentario en una linea 
+/* comentarios en bloque:
+* Console writeLine es para imprimir 
+* para imprimir texto sin variable
+* tiene que ir entre " " comi*/
+using System;
+namespace p-1
+    {
+        internal class program // paso 1 
+        {
+            static void main(string[] args)
+            {
+                //DECLARAR VARIABLES
+                string nombre = ("Hector");
+                //imprimir nombre referenciado a una variable 
+                Console.WriteLine("Hector");
+                //cada lineas debe finalizar con ; punto y coma
+            }
+        }
+    }
+
