d0cdbaa6cd26ec23dc0230015bf8340d1f58ae20
Mafuyuwu49 <146400599+Mafuyuwu49@users.noreply.github.com>
2023-10-01 16:19:48 -0600

    Add files via upload

---
+++ Unidad 2/P_2.cs
@@ -0,0 +1,21 @@
+﻿//Comentarios en una linea
+/*Comentarios en bloque:
+ * Console en Writeline para imprimir
+ * Para imprimir texto sin variable
+ * tiene que ir entre "" comillas dobles*/
+using System;
+
+namespace P_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //algoritmo 2
+            int a = 6; int b = 7;
+            int suma = 0;
+            suma = a + b;
+            Console.WriteLine(suma);
+        }
+    }
+}
