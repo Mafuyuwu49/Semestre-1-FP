201114af6542a46c7a83e1447904590d9893c29a
Mafuyuwu49 <146400599+Mafuyuwu49@users.noreply.github.com>
2023-10-01 21:42:52 -0600

    P_6.cs

--- Unidad 2/P_6.cs
+++ Unidad 2/P_6.cs
@@ -7,10 +7,10 @@ namespace P_6
         static void Main(string[] args)
         {
             int C, F;
-            Console.WriteLine("Introduce el valor de los grados centigrados");
+            Console.WriteLine("Inserte el valor de los grados centigrados");
             C = Convert.ToInt32(Console.ReadLine());
             F = (C * 9/5) + 32;
-            Console.WriteLine("Los grados fahrenheit son :" + F);
+            Console.WriteLine("Los grados en fahrenheit son :" + F);
 
         }
     }
