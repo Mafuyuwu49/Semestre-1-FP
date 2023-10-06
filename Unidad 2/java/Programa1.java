fea115a534f8bef92c384bcbfaf79009a573f795
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-06 15:21:37 -0600

    Programa 1 java

---
+++ Unidad 2/java/Programa1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1;
+import java.lang.*;
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa1 {
+
+    public static void main(String[] args) {
+        String nombre="Ayari Daniela";
+        System.out.println(nombre);
+    }
+}
