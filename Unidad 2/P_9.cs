0032425da72f9bf7293d19cd5b38996c55664539
Mafuyuwu49 <146400599+Mafuyuwu49@users.noreply.github.com>
2023-10-09 00:30:13 -0600

    P_9.cs

--- Unidad 2/P_9.cs
+++ Unidad 2/P_9.cs
@@ -7,24 +7,24 @@ using System;
 
 namespace P_9 
 {
-    internal class Calculadora
+    internal class Programa 9
     {
         static void Main(string[] args)
         {
-            int a, b, s, r, m, d;
+            int a, b, l, o, v, e;
             Console.WriteLine("Introduce primer valor");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Introduce segundo valor");
             b = Convert.ToInt32(Console.ReadLine());
 
             s = a + b;
-            Console.WriteLine(" Suma es igual a :" + s);
+            Console.WriteLine(" Suma es igual a :" + l);
             r = a - b;
-            Console.WriteLine(" Resta es igual a :" + r);
+            Console.WriteLine(" Resta es igual a :" + o);
             m = a * b;
-            Console.WriteLine(" Multiplicación es igual a :" + m);
+            Console.WriteLine(" Multiplicación es igual a :" + v);
             d = a / b;
-            Console.WriteLine(" División es igual a :" + d);
+            Console.WriteLine(" División es igual a :" + e);
         }
     }
 }
