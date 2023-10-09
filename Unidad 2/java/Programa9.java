2b902521821471593045130da5d0757419f8ad2d
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-09 01:03:56 -0600

    new

---
+++ Unidad 2/java/Programa9.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa_9;
+
+import java.util.Scanner;
+
+
+public class Programa 9 {
+    public static void main(String[] args) {
+        int a = 1, b = 2, l, o, v, e;
+        
+        l = a + b;
+        System.out.println("La suma es igual a: " + l);
+        o = a - b;
+        System.out.println("La resta es igual a: " + o);
+        v = a * b;
+        System.out.println("La multiplicación es igual a: " + v);
+
+        if (b != 0) {
+            e = a / b;
+            System.out.println("La división es igual a: " + e);
+        } else {
+            System.out.println("No se puede dividir por cero.");
+        }
+    }
+}
