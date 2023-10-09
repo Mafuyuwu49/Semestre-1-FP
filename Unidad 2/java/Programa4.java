2c1414f652e2eee49b86d41f91cec8de47fed20c
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-09 00:14:04 -0600

    new

---
+++ Unidad 2/java/Programa4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa_4;
+
+import java.util.Scanner;
+
+public class Programa 4 {
+    public static void main(String[] args) {
+        int y, a;
+        int suma = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escriba el valor de a: ");
+        a = scanner.nextInt();
+        System.out.println("Escriba el valor de y: ");
+        y = scanner.nextInt();
+
+        suma = y + a + 3;
+        System.out.println("El valor de x es: " + suma);
+    }
+}
+
+
+
+
+
+
