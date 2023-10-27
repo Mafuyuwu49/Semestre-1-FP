02174dd457b6556f92a6c8cf64f8803f4d38551f
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-27 18:07:23 -0400

    new

---
+++ Unidad 3/java unidad 3/Proyecto17u3.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.proyecto17u3;
+
+import java.util.Scanner;
+public class Proyecto17u3 {
+
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                char opcion;
+                do{
+                    System.out.println("Bienvenido a una sola ejecucion!");
+                    System.out.println("Escribe n para salir!!");
+                    opcion=sc.next().charAt(0);
+                }while(opcion != 'n' ); 
+
+        
+    }
+}
