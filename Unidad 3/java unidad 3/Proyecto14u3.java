9e5fe676fe0e4a3060be6c85a110315115c29a73
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-30 20:56:31 -0600

    new

---
+++ Unidad 3/java unidad 3/Proyecto14u3.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.proyecto14u3;
+
+
+public class Proyecto14u3 {
+
+  
+    public static void main(String[] args) {
+        int numero = 2;
+        int contador = 0;
+
+        for (; contador < 100; contador++) {
+            System.out.println(numero);
+            numero += 2;
+        }
+    }
+}
