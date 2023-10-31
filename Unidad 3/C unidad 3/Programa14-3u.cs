761ab0b6000266fc0e81717024c03d93b761528a
Mafuyuwu49 <L23180778@slp.tecnm.mx>
2023-10-30 23:07:14 -0600

    new

---
+++ Unidad 3/C unidad 3/Programa14-3u.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Programa14-3u
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2;
+            int contador = 0;
+
+            while (contador < 100)
+            {
+                Console.WriteLine(numero);
+                numero += 2;
+                contador++;
+            }
+        }
+    }
+}
